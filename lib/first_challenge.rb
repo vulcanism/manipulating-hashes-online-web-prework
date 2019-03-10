228b931f099d845249630824b95647f592e229c0
Savannah C <sgcorrell@gmail.com>
2019-03-10 16:11:39 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-  contacts["Jon Snow"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
+  
 
 
   #remember to return your newly altered contacts hash!
